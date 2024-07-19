5405ec5b17bbece297ffc1f5ab3bfd911830b7cd
joao victor <joaovprudencio13@gmail.com>
2024-07-19 04:38:31 -0300

    construção do header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return(
+        <div>
+          <nav>
+            <ul>
+                <li>Serviços</li>
+                <li>home</li>
+                <li>horarios</li>
+            </ul>
+          </nav>
+        </div>
+    )
+}
+
+export default Header
