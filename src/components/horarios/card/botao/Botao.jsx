2a0d181b5943c1697ec51190108a43f9035a5461
joao victor <joaovprudencio13@gmail.com>
2024-07-24 06:29:29 -0300

    criando arquivos para componente

---
+++ src/components/horarios/card/botao/Botao.jsx
@@ -0,0 +1,11 @@
+import Botao from './Botao.module.css';
+
+const Botao = () =>{
+     <div>
+        <button>
+            <p>Marcar Horario</p>
+        </button>
+     </div>
+}
+
+export default Botao;
