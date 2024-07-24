4d703fef560dbea8395d0dae596094964130dcf3
joao victor <joaovprudencio13@gmail.com>
2024-07-24 05:44:06 -0300

    criando card

--- src/components/horarios/card/Marcar.jsx
+++ src/components/horarios/card/Marcar.jsx
@@ -1,10 +1,18 @@
-import styles from './Marcar.module.css'
-const Marcar = ()=>{
-return(
-    <div>
-
+import styles from "./Marcar.module.css";
+import relogio from "../img/relogioCerto.png";
+const Marcar = () => {
+  return (
+    <div className={styles.corpo}>
+      <div className={styles.card}>
+        <div className={styles.horarios}>
+          <img className={styles.image} src={relogio} alt="" />
+          <h1 className={styles.mensagemHorarios}>horarios</h1>
+        </div>
+        <h3 className={styles.semana}>segunda á sabado</h3>
+        <h5 className={styles.horas}>9:00 -12:00 ás 14:00 - 20:00</h5>
+      </div>
     </div>
-)
-}
+  );
+};
 
-export default Marcar
+export default Marcar;
--- src/components/horarios/horarios.jsx
+++ src/components/horarios/horarios.jsx
@@ -1,14 +1,17 @@
 import styles from "./horarios.module.css";
 import Preco from "./preços/Preco";
+import Marcar from './card/Marcar';
+
 const Horarios = () => {
   return (
     <div className={styles.corpo}>
+      
       <div className={styles.tabela}>
         <div className={styles.precos}>
           <Preco/>
-        </div>
       </div>
-      
+      </div>
+     <Marcar/>
     </div>
   );
 };
