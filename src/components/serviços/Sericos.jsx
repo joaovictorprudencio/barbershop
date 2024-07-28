1ab059f89c0e81af5ce7a51d4d0a285226f0f11a
joao victor <joaovprudencio13@gmail.com>
2024-07-28 19:20:46 -0300

    criação do componente de teste

--- src/components/serviços/Sericos.jsx
+++ src/components/serviços/Sericos.jsx
@@ -1,14 +1,14 @@
 import CardService from "./cardServiços/CardService";
 import Footer from '../footer/Footer'
 import styles from './Servicos.module.css'
-
+import corteTesoura from './img/corteTesoura.png'
 const Servicos = () => {
   return(
     <div className={styles.paginaService} >
         <CardService 
-         titulo="Serviço de Barbeiro" 
-         imagemSrc="https://via.placeholder.com/150" 
-          detalhamento="Detalhamento do serviço de barbeiro."
+         titulo="corte na tesoura" 
+         imagem ={corteTesoura}
+          texto="Aparo, remodelação e remoção completa da barba com navalha, proporcionando melhor acabamento.."
          />
         <CardService/>
         <CardService/>
--- src/components/serviços/cardServiços/CardService.jsx
+++ src/components/serviços/cardServiços/CardService.jsx
@@ -1,11 +1,14 @@
 import styles from "./CardService.module.css";
 
-const CardService = ({titulo, imagem , texto}) => {
+const CardService = ({ titulo, imagem, texto }) => {
   return (
     <div className={styles.componente}>
-      <h3 className={styles.titulo}>{titulo}</h3>
       <img className={styles.imagem} src={imagem} alt="" />
-      <p className={styles.texto}>{texto}</p>
+
+      <div className={styles.content}>
+        <h3 className={styles.titulo}>{titulo}</h3>
+        <p className={styles.texto}>{texto}</p>
+      </div>
     </div>
   );
 };
