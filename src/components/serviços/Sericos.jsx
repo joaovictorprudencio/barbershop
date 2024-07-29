9d894de0a869d27abcf0c40bf606aa40862ea3ee
joao victor <joaovprudencio13@gmail.com>
2024-07-29 07:26:07 -0300

    pagina de serviços finalizada

--- src/components/serviços/Sericos.jsx
+++ src/components/serviços/Sericos.jsx
@@ -2,16 +2,32 @@ import CardService from "./cardServiços/CardService";
 import Footer from '../footer/Footer'
 import styles from './Servicos.module.css'
 import corteTesoura from './img/corteTesoura.png'
+import barba from './img/barba.png'
+import degradeNavalhado from './img/degrade.jfif'
+import social from './img/corteSocial.png'
 const Servicos = () => {
   return(
     <div className={styles.paginaService} >
+         <CardService 
+         titulo="degrade navalhado" 
+         imagem ={degradeNavalhado}
+          texto="O corte na tesoura tem uma melhor remodelação, proporcionando um acabamento natural e detalhado."
+         />
         <CardService 
          titulo="corte na tesoura" 
          imagem ={corteTesoura}
-          texto="Aparo, remodelação e remoção completa da barba com navalha, proporcionando melhor acabamento.."
+          texto="O corte na tesoura tem uma melhor remodelação, proporcionando um acabamento natural e detalhado."
          />
-        <CardService/>
-        <CardService/>
+        <CardService
+        titulo="barba"
+        imagem={barba}
+        texto=' Aparo, remodelação e remoção completa da barba com navalha, proporcionando melhor acabamento.'
+        />
+        <CardService
+           titulo="corte social"
+           imagem={social}
+           texto=' Aparo, remodelação e remoção completa da barba com navalha, proporcionando melhor acabamento.'
+        />
         <Footer/>
         
     </div>
--- src/components/serviços/cardServiços/CardService.jsx
+++ src/components/serviços/cardServiços/CardService.jsx
@@ -4,7 +4,6 @@ const CardService = ({ titulo, imagem, texto }) => {
   return (
     <div className={styles.componente}>
       <img className={styles.imagem} src={imagem} alt="" />
-
       <div className={styles.content}>
         <h3 className={styles.titulo}>{titulo}</h3>
         <p className={styles.texto}>{texto}</p>
