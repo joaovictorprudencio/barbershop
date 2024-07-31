d8801a803326e7c186d3e97f6237114e10794fc7
joao victor <joaovprudencio13@gmail.com>
2024-07-31 17:11:17 -0300

    finalizando tela inicial

--- src/components/home/promocao/Promocao.jsx
+++ src/components/home/promocao/Promocao.jsx
@@ -1,11 +1,13 @@
-
+import tesoura from '../img/promo.png'
 import styles from "./Promocao.module.css"
 const Promocao = ()=>{
     return(
         <div className={styles.promocao}>
              <h1 className={styles.titulo}>promoções</h1>
             <div className={styles.promocoes}>
-               <img src="" alt="" />
+               <img className={styles.tesoura} src={tesoura} alt="" />
+               <p className={styles.texto}>corte navalhado + barba : 45,00</p>
+               <p className={styles.texto}>corte navalhado + barba : 45,00</p>
             </div>
         </div>
     )
