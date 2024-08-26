0605b80a4359e00df7545c8462ff9583fdcbb2d9
joao victor <joaovprudencio13@gmail.com>
2024-08-26 13:53:48 -0300

    correção para novo item do card

--- src/components/home/promocao/Promocao.jsx
+++ src/components/home/promocao/Promocao.jsx
@@ -1,16 +1,16 @@
-import tesoura from '../img/promo.png'
-import styles from "./Promocao.module.css"
-const Promocao = ()=>{
-    return(
-        <div className={styles.promocao}>
-             <h1 className={styles.titulo}>promoções</h1>
-            <div className={styles.promocoes}>
-               <img className={styles.tesoura} src={tesoura} alt="" />
-               <p className={styles.texto}>corte navalhado + barba : 45,00</p>
-               <p className={styles.texto}>corte navalhado + barba : 45,00</p>
-            </div>
-        </div>
-    )
-}
+import tesoura from "../img/promo.png";
+import styles from "./Promocao.module.css";
+const Promocao = () => {
+  return (
+    <div className={styles.promocao}>
+      <h1 className={styles.titulo}>promoções</h1>
+      <div className={styles.promocoes}>
+        <img className={styles.tesoura} src={tesoura} alt="" />
+        <p className={styles.texto}>corte navalhado + barba : 45,00</p>
+        <p className={styles.texto}>corte navalhado + barba : 45,00</p>
+      </div>
+    </div>
+  );
+};
 
 export default Promocao;
--- src/components/horarios/card/Marcar.jsx
+++ src/components/horarios/card/Marcar.jsx
@@ -1,17 +1,16 @@
 import styles from "./Marcar.module.css";
-import relogio from "../img/relogioCerto.png";
-import Botao from "./botao/Botao";
+
 const Marcar = () => {
   return (
     <div className={styles.corpo}>
       <div className={styles.card}>
         <div className={styles.horarios}>
-          <img className={styles.image} src={relogio} alt="" />
-          <h1 className={styles.mensagemHorarios}>horarios</h1>
+          <h1 className={styles.mensagemHorarios}>horarios de funcionamento</h1>
+
+          
         </div>
         <h3 className={styles.semana}>segunda á sabado</h3>
         <h5 className={styles.horas}>9:00 -12:00 ás 14:00 - 20:00</h5>
-        <Botao/>
       </div>
     </div>
   );
