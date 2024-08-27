17601fdbfdf950bde4ee938b58bfc229f09306e5
joao victor <joaovprudencio13@gmail.com>
2024-08-27 05:53:08 -0300

    estilizando form

--- src/components/TelaMarcar/TelaMarcar.jsx
+++ src/components/TelaMarcar/TelaMarcar.jsx
@@ -1,9 +1,30 @@
-const TelaMarcar = () =>{
-    return(
-        <div>
-            <h1> funcionando</h1>
+import styles from "./TelaMarcar.module.css";
+import calendario from "./img/calendario.png"
+const TelaMarcar = () => {
+  return (
+    <div className={styles.componentTela}>
+
+      <div className={styles.cardForm}>
+
+        <div className={styles.titulo}>
+          <img className={styles.icon} src={calendario} alt="" /> 
+          <h2>agendamento</h2> 
         </div>
-    )
+
+
+        <div className={styles.contentData}>
+            <p>para qual data ?</p>
+           <input type="date" name="" id="" /> 
+        </div>
+       
+
+        
+
+
+      </div>
+
+    </div>
+  );
 };
 
 export default TelaMarcar;
