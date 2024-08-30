bcab1f0572de02efc8f0bb5bf33c0575c7cc8706
joao victor <joaovprudencio13@gmail.com>
2024-08-30 11:10:02 -0300

    criando componentes de inputs

---
+++ src/components/TelaMarcar/confirmação/CardConfirm.jsx
@@ -0,0 +1,10 @@
+import styles from "./CardConfirm.module.css";
+
+const CardConfirm = () => {
+    return (
+        <div className={styles.component}>
+
+        </div>
+    )
+}
+export default CardConfirm
