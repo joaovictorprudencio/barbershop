f216583780d937c73c8f829ed6299c6fe9467c6e
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-02-06 23:10:00 -0300

    creating component service order

---
+++ src/components/service-order/serviceOrder.jsx
@@ -0,0 +1,15 @@
+import style from "./serviceOrder.module.css"
+
+
+const serviceOrderList = () => {
+
+
+
+  return (
+    <div className={style.page}>
+         <h1> service order is work</h1>
+    </div>
+  )
+}
+
+export default serviceOrderList;
