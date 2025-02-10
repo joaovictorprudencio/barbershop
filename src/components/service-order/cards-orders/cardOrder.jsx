2b050dfbb8e31b836fb6feb61df0494eef07d5d7
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-02-10 08:43:30 -0300

    create card component

---
+++ src/components/service-order/cards-orders/cardOrder.jsx
@@ -0,0 +1,12 @@
+
+import style from "./cardOrder.module.css"
+
+const CardOrder = ({name,number,date,time}) => {
+  return (
+    <div className={style.card}>
+        <h1>componente card is work</h1>
+    </div>
+  );
+}
+
+export default CardOrder
