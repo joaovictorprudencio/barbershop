55a073b4db7930688643b3c1a0ddf260b2fdc660
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-02-10 15:10:38 -0300

    creating conditional render

--- src/components/service-order/serviceOrder.jsx
+++ src/components/service-order/serviceOrder.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-unused-vars */
 import style from "./serviceOrder.module.css"
 import ServiceOrder from "./cards-orders/cardOrder"
 import Footer from "../footer/Footer"
@@ -7,32 +9,46 @@ const serviceOrderList = () => {
 
   const [loading, setLoading] = useState(false);
 
+  const services = ["ok"]
+
   return (
     <div className={style.page}>
-      <section className={style.content}>
-        <div className={style.list}>
-             
-             <h2>Horarios para hoje</h2>
-        
-          <ServiceOrder
-            name="João" 
-            date="10/02"
-            number="97323892"
-            time="9:00"
-          />
-          <ServiceOrder
-            name="João"
-            date="10/02"
-            number="97323892"
-            time="9:00"
-          />
-    
-   
-      
-        </div>
 
+ <section className={style.content}>
+       
+
+      
+      {services.length === 0 && (
+        <div className={style.noServices}>
+          <img 
+          src="/public/Search.gif"
+          alt="Nenhum horário disponível"
+          className={style.notfound}
+           />
+          <p>Sem horários disponíveis para hoje.</p>
+        </div>
+      )}
+
+     
+      
+
+          
+          {services.length > 0 &&
+            services.map((service, index) => ( 
+              <>  <div className={style.list}>
+              
+              <h2>Horários para hoje</h2><ServiceOrder
+                key={index}
+                name="João"
+                date="10/02"
+                number="97323892"
+                time="9:00" />  </div></>
+            ))
+          }
+      
       </section>
-      <Footer/>
+
+      <Footer />
     </div>
   )
 }
