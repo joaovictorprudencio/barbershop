941690820dddb92b69835f3d1b5d4bb36c3dd22d
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-02-12 15:22:29 -0300

    dcreate request for server

--- src/services/list-times-today-service.js
+++ src/services/list-times-today-service.js
@@ -1,2 +1,34 @@
+/* eslint-disable no-undef */
 
 
+const ListTimesToday = async () => {
+
+    try {
+        const token = localStorage.getItem("token");
+
+        const response = await fetch(`${import.meta.env.API_URL}/list`, {
+
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if(!response.ok) {
+            errorData = await response.json();
+            throw new Error(errorData.messege || "Sem conexão ")
+        }
+
+        const data = await response.json();
+
+        return data
+
+    } catch (err) {
+         return err.messege
+    }
+
+
+}
+
+export default ListTimesToday
