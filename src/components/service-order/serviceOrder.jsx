bb04e8478d3d86cc295374f0d9040b0ec5300249
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-02-12 14:11:28 -0300

    adding coditional styles

--- src/components/service-order/serviceOrder.jsx
+++ src/components/service-order/serviceOrder.jsx
@@ -9,43 +9,46 @@ const serviceOrderList = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const services = ["ok"]
+  const [status , setstatus] = useState("sucess");
+
+  const services = [""]
 
   return (
     <div className={style.page}>
 
- <section className={style.content}>
-       
+      <section className={style.content}>
 
-      
-      {services.length === 0 && (
-        <div className={style.noServices}>
-          <img 
-          src="/public/Search.gif"
-          alt="Nenhum horário disponível"
-          className={style.notfound}
-           />
-          <p>Sem horários disponíveis para hoje.</p>
-        </div>
-      )}
+      <div className={
+        `${style.list} ${status === "sucess" ? style.list: ""} ${status ==="error" ? style.listNotFound : ""}`
+        }>
 
-     
-      
+        <h2 className={style.title}>Horarios Marcados </h2>
 
-          
-          {services.length > 0 &&
-            services.map((service, index) => ( 
-              <>  <div className={style.list}>
-              
-              <h2>Horários para hoje</h2><ServiceOrder
+        {services.length === 0 && (
+          <div className={style.noServices}>
+            <img
+              src="/Search.gif"
+              alt="Nenhum horário disponível"
+              className={style.notfound}
+            />
+            <p className={style.messegeNotfound}>Sem horários disponíveis para hoje.</p>
+          </div>
+        )}
+
+
+        {services.length > 0 &&
+          services.map((service, index) => (
+            <> 
+
+              <ServiceOrder
                 key={index}
                 name="João"
                 date="10/02"
                 number="97323892"
-                time="9:00" />  </div></>
-            ))
-          }
-      
+                time="9:00" />  </>
+          ))
+        }
+</div>
       </section>
 
       <Footer />
