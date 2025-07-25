83d175cb6de620b8c2716660c1a11b690ef6cf9c
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-07-24 22:41:58 -0300

    correção

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
       const data = await loginBarber(login.email, login.password);
 
 
-      localStorage.setItem('authToken', data.token);
+    
 
 
      navigate('/agendamentos');
