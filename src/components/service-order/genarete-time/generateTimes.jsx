ab732c9bd1551ed6d776395b2d5ea0c663b75427
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-08-15 12:53:51 -0300

    adicionando retorno visual

--- src/components/service-order/genarete-time/generateTimes.jsx
+++ src/components/service-order/genarete-time/generateTimes.jsx
@@ -1,21 +1,26 @@
 import style from "./generateTimes.module.css"
 import axios from "axios";
-
+import Modal from "../../modal/modal";
 const GenerateTimes = () => {
 
     const generateTime = () => {
+        
         return axios.get(`${import.meta.env.VITE_API_URL}/times/config`, {
             headers: {
                 "Content-Type": "application/json",
             }
         });
-
     }
+
+    const actionButton = () => {
+      generateTime();
+      alert("Rotina gerada !")
+    };
 
     return (
         <div class={style.component}>
             <section class={style.content}>
-                <button class={style.button} onClick={generateTime}>
+                <button class={style.button} onClick={actionButton}>
                     Gerar Rotina
                 </button>
             </section>
